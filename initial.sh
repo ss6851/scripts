ceb0639ce79ad379ecec1ed1c0ea037b66e0a8b1
root <root@kali.kali.org>
2020-05-20 18:10:17 -0500

    W/ updated UDP scan

--- initial.sh
+++ initial.sh
@@ -3,22 +3,22 @@
 if [ "$#" = "1" ]; then
 	mkdir nmap;clear
 	printf "[+] Performing the default scan\n\n"
-	nmap -Pn -sC -sV -oN nmap/initial $1
+	nmap -Pn -sC -sV -oN nmap/initial.nmap $1
 
 	printf "\n[+] Performing an all port scan\n\n"
-	nmap -Pn -p1-65535 -oNG nmap/allports $1
+	nmap -Pn -p1-65535 -oG nmap/allports.gnmap $1
 
 	ports=$(grep -oP ' [\d]{1,6}/' nmap/allports.gnmap | sed 's/\///g' | tr '\n' ',')
 
 	printf "\n[+] Performing a targetted port scan\n\n"
 
-	nmap -Pn -p"${ports} " -sC -sV -oN nmap/targetted $1
+	nmap -Pn -p"${ports}" -sC -sV -oN nmap/targetted.nmap $1
 
 	printf "\n\n [+] Performing an all ports vuln detection scan\n\n"
-	nmap -Pn -p"${ports} " --script vuln -oN nmap/vuln $1
+	nmap -Pn -p"${ports}" --script vuln -oN nmap/vuln.nmap $1
 
 	printf "\n\n[+] Performing a UDP vuln scan\n\n"
-	nmap -Pn -p- -sU -sC -sV -oA nmap/udp $1
+	nmap -Pn -vvv -sU -oN nmap/udp.nmap $1
 else
 	printf "[+] Usage: ./initial.sh <IP-Address>\n"
 fi
