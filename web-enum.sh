1d49d503021452c0a9efac77a296d7d9f0090d52
root <root@kali.kali.org>
2020-06-08 17:02:56 -0500

    printf line modified

--- web-enum.sh
+++ web-enum.sh
@@ -7,14 +7,14 @@ if [ "$#" == "2" ]; then
 
 	mkdir www-enum; clear
 
-	printf "${Y}[+]] Performing Nikto scan on port $2 ${NC}\n\n"
+	printf "${Y}[+] Performing Nikto scan on port $2 ${NC}\n\n"
 	nikto -host $1:$2 | tee www-enum/nikto$2.txt
 
-	printf "\n\n${Y}[+]] Performing dir scan w/ extensions on port $2 ${NC}\n\n"
+	printf "\n\n${Y}[+] Performing dir scan w/ extensions on port $2 ${NC}\n\n"
 	gobuster -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt dir -f -x php,sh,aspx,asp,config,txt,cgi -u http://$1:$2 -o www-enum/gobuster$2.ext.out
 
-	printf "\n\n${Y}[+]] Performing normal dir scan on port $2 ${NC}\n\n"
+	printf "\n\n${Y}[+] Performing normal dir scan on port $2 ${NC}\n\n"
 	gobuster -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt dir -u http://$1:$2 -o www-enum/gobuster$2.dir.out
 else
-	printf "[${Y}[+]] Usage: /opt/script/./web-enum.sh <IP-Address> <Port Number>${NC}\n"
+	printf "[${Y}[+] Usage: /opt/script/./web-enum.sh <IP-Address> <Port Number>${NC}\n"
 fi
