318476c7493da791cc7c95438b89cc43db34f12c
root <root@kali.kali.org>
2020-06-15 20:17:11 -0500

    smartly stop vpn and web server

--- firesuitevpn.sh
+++ firesuitevpn.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/bash
-# Ask for the vpn type
+
 pkill -9 openvpn
-pkill -9 python3
+pid=$(ps aux | grep http.server | awk '{print $2}' | head -n 1) && kill -9 ${pid}
 clear
 
 echo Enter the VPN name: htb, vhl, thm
-
 read vpn
 
 echo Enter the directory name
-
 read dir
 
 if [[ ${vpn} == "htb" ]]
 then
+	thmvpn_pid=$(ps aux | grep ss6851.ovpn | awk '{print $2}' | head -n 1) && kill -9 ${thmvpn_pid}
 	openvpn /opt/vpn/ss2016.ovpn &
 	mkdir -p /root/htb/${dir}/exploits
 	cd /root/htb/${dir}/exploits && python3 -m http.server 80 &
 
 elif [[ ${vpn} == "thm"  ]]
 then 
+	htbvpn_pid=$(ps aux | grep ss2016.ovpn | awk '{print $2}' | head -n 1) && kill -9 ${htbvpn_pid}
 	openvpn /opt/vpn/ss6851.ovpn &
 	mkdir -p /root/thm/${dir}/exploits
 	cd /root/thm/${dir}/exploits && python3 -m http.server 80 &
